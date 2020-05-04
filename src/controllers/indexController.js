430e378d15fec872a43f7f5a2326325f478e72ea
bonino97 <juanbonino97@gmail.com>
2020-05-03 22:17:27 -0300

    Update indexController.js

--- src/controllers/indexController.js
+++ src/controllers/indexController.js
@@ -341,9 +341,6 @@ const ExecuteMonitoring = async (req,res) => {
                                             });
                                         });
         }
-    
-        let gobusterExecuted = true;
-
 
         if(gobusterExecuted){
 
